042ff31179d91784dd94c4650164c05df581f5b9
Isak <isakhorvath@gmail.com>
2019-11-08 21:57:13 +0100

    fixed bullet-rotation

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour {
     public GameObject bullet;
+    float angle;
 
     private bool shooting = true;
 
     void Start() {
         float deltaX = transform.parent.position.x - transform.position.x;
         float deltaY = transform.parent.position.y - transform.position.y;
-        float angle = Mathf.Atan2(deltaY, deltaX);
+        angle = Mathf.Atan2(deltaY, deltaX);
         transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, angle);
     }
 
     IEnumerator Shoot() {
         shooting = false;
         GameObject temp = Instantiate(bullet, transform.position, Quaternion.identity);
-        temp.GetComponent<Bullet>().zRotation = transform.rotation.z;
+        temp.GetComponent<Bullet>().zRotation = (Mathf.Rad2Deg * transform.rotation.z)*2 + Mathf.PI;
         yield return new WaitForSeconds(1.5f);
         shooting = true;
     }
