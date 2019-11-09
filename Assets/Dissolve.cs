e89ee1ec68db7264ebb48ff74b3098e9a9c1f2c5
Isak <isakhorvath@gmail.com>
2019-11-09 20:13:30 +0100

    planet parts

---
+++ Assets/Dissolve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    Vector3 direction;
+
+    void Start()
+    {
+        direction = Random.insideUnitCircle.normalized;
+    }
+
+    void Update()
+    {
+        transform.position += direction * Time.deltaTime * 2.5f;
+        transform.Rotate(0, 0, -345.0f * Time.deltaTime, Space.Self);
+    }
+}
