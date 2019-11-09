a39165c6bcf5b8ed99ba6860ae20fe7b75541a96
Isak <isakhorvath@gmail.com>
2019-11-09 08:49:58 +0100

    added simulate-button

---
+++ Assets/Planet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+    bool simulating = false;
+
+    public void Simulate() {
+        simulating = true;
+    }
+
+    void Update() {
+        if (simulating) {
+            transform.Rotate(0, 0, -45.0f * Time.deltaTime, Space.Self);
+        }
+    }
+}
