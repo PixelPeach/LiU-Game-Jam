5586e05835352d76d0709b604825ea34e828d15e
Isak <isakhorvath@gmail.com>
2019-11-10 09:16:06 +0100

    fixed cracks instead of healthbar

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Healthbar : MonoBehaviour {
 
+    public Sprite[] cracks;
+    public SpriteMask mask;
+
     public float health = 1.0f;
     public bool isDead = false;
 
@@ -15,6 +18,11 @@ public class Healthbar : MonoBehaviour {
         else {
             isDead = true;
         }
+
+        if (health > 0.75f && health < 0.85f) { mask.sprite = cracks[0]; }
+        if (health > 0.55f && health < 0.75f) { mask.sprite = cracks[1]; }
+        if (health > 0.35f && health < 0.45f) { mask.sprite = cracks[2]; }
+        if (health > 0.15f && health < 0.25f) { mask.sprite = cracks[3]; }
     }
 
 }
