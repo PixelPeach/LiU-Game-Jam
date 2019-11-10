c23310f387ba51a492599017f45611f492acfca5
Isak <isakhorvath@gmail.com>
2019-11-10 11:03:15 +0100

    Commented @ELEMENTLHERO ' s scripts you should totaly take a look at those

--- Assets/Scripts/Cosmetics/DistortOverTime.cs
+++ Assets/Scripts/Cosmetics/DistortOverTime.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 
 public class DistortOverTime : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //What is this script used for?
+
     public float animationSpeed = 2f;
     private float timer = 0;
 
-    void Start()
-    {
-    }
-
     void Update()
     {
+        //Increase with Time.deltaTime and scale with multiplier field.
         timer += 0.01f;
-        float innerscaleX = 0.15f + (float)((Mathf.Sin((float)(timer * animationSpeed))) * 0.008f);
+
+        //Make 0.15f float offset
+        //No need to cast as floats because Sin function return floats and fields already float.
+        float innerscaleX = 0.15f + (Mathf.Sin((timer * animationSpeed))) * 0.008f;
+
+        //Do the same thing here
         float innerscaleY = 0.15f + (float)((Mathf.Sin((float)(timer * animationSpeed + Mathf.PI))) * 0.01f);
 
+        //Use Vector2 if youre not going to change the z-axis
         transform.localScale = new Vector3(-innerscaleX, innerscaleY, 1);
     }
 }
--- Assets/Scripts/Cosmetics/StarScript.cs
+++ Assets/Scripts/Cosmetics/StarScript.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class StarScript : MonoBehaviour
 {
+    //This script is obsolete, wrote another one
+
     public SpriteRenderer Renderer;
     public Vector2 Speed = new Vector2(0.0004f,0.00015f);
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        //Could write it as Renderer.size += Speed instead
         Renderer.size = Renderer.size + Speed;
     }
 }
